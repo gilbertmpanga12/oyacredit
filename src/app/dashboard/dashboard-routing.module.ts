d32030211afacc8597c6ae29b157d19acbdac8b8
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-10-24 17:02:20 +0300

    Update;- set up routes for feature routes

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -4,7 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import {HomeComponent} from './home/home.component';
 import {HistoryComponent} from './history/history.component';
-
+import {BanksComponent} from './banks/banks.component';
+import {AirtimeComponent} from './airtime/airtime.component';
+import {MobilemoneyComponent} from './mobilemoney/mobilemoney.component';
+import {MobiledataComponent} from './mobiledata/mobiledata.component'
 const routes: Routes = [{ path: '', component: DashboardComponent,
 children:[
 {
@@ -14,6 +17,22 @@ children:[
 {
   path: 'history',
   component: HistoryComponent
+},
+{
+  path: 'banks',
+  component: BanksComponent
+},
+{
+  path: 'airtime',
+  component: AirtimeComponent
+},
+{
+  path: 'mobile-money',
+  component: MobilemoneyComponent
+},
+{
+  path: 'mobile-data',
+  component: MobiledataComponent
 }
 ] }];
 
