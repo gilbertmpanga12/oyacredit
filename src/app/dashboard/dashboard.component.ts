17abdeb217c41d15e4049ebed64628fee7f90e7b
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-10-24 16:29:27 +0300

    Update;- restored working components

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Menu } from '../models/models';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class DashboardComponent implements OnInit {
- 
+  howFiller: boolean = false;
+  features: Menu[] = [
+    {
+      name: 'Home',
+      icon: 'house'
+    },
+    {
+      name: 'Airtime',
+      icon: 'call'
+    },
+    {
+      name: 'Mobile Money',
+      icon: 'smartphone'
+    },
+    {
+      name: 'Shop',
+      icon: 'shopping_bag'
+    }
+  ];
+  others: Menu[] = [
+    {
+      name: 'History',
+      icon: 'history'
+    },
+    {
+      name: 'Sign out',
+      icon: 'login'
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
